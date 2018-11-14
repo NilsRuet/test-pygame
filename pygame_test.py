ddc2585cd429da54922b35a520e1703902adaa5b
Nils <44983884+NilsRuet@users.noreply.github.com>
2018-11-14 21:24:24 +0100

    Add files via upload

---
+++ pygame_test.py
@@ -0,0 +1,36 @@
+import pygame
+import time
+import sys
+pygame.init()
+
+delai = 1/30;
+
+size = width, height = 600, 240
+speed = [1, 1]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            speed[0]*=2
+            speed[1]*=2
+        elif event.type == pygame.KEYUP:
+            speed[0]*=0.5
+            speed[1]*=0.5
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+    
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+    time.sleep(delai);
